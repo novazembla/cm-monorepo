590d46bece06c105d12fb6439691230cd23777f1
Novazembla <nova@pro-pro.local>
2022-03-23 11:38:56 +0100

    Added Location Suggestion notification

--- packages/api/src/services/serviceLocation.ts
+++ packages/api/src/services/serviceLocation.ts
@@ -9,6 +9,43 @@ import {
   geocodingGetAddressCandidates,
   geocodingGetBestMatchingLocation,
 } from "../utils/geocoding";
+import { getApiConfig } from "../config";
+import { sendEmail } from "./serviceEmail";
+import { getSettings } from "./serviceSetting";
+
+export const sendEmailSuggestionConfirmation = async (id: number | string) => {
+  const apiConfig = getApiConfig();
+  const settings = await getSettings("settings");
+
+  if (!settings?.contactEmail) return;
+
+  let subject = "";
+  let body = "";
+
+  const url = `${apiConfig.baseUrl.backend}/locations/update/${id}`;
+
+  if (apiConfig.defaultLanguage === "en") {
+    subject = `${apiConfig.email.subjectPrefix} New map location suggestion`;
+    body = `Dear user,
+
+Someone has posted a new location suggestion. Please review it using the following link: ${url}
+
+Thank you your, 
+${apiConfig.appName} team`;
+  }
+
+  if (apiConfig.defaultLanguage === "de") {
+    subject = `${apiConfig.email.subjectPrefix} Neuer Kartenpunktvorschlag`;
+    body = `Liebe NutzerIn,
+
+Soeben wurde ein neuer Kartenpunktvorschlag abgesendet. Bitte Prüfen Sie diesen über den folgenden Link: ${url}
+
+Vielen Dank, 
+Ihr ${apiConfig.appName} Team`;
+  }
+
+  await sendEmail(settings?.contactEmail, subject, body);
+};
 
 export const locationSuggestionCreate = async (
   data: any
@@ -64,6 +101,10 @@ export const locationSuggestionCreate = async (
       ...data?.heroImage?.update,
       type: "image",
     });
+  }
+
+  if (location?.id) {
+    await sendEmailSuggestionConfirmation(location.id);
   }
   return location;
 };
