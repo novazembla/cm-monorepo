ca596ec3df7baa96f6ae95905fd09e795a8f1f63
Vincent Van Uffelen <hi@vincentvanuffelen.com>
2024-11-26 21:07:26 +0100

    chore: adds better logging

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -29,6 +29,17 @@ import {
   getGeoJson,
 } from "./routes";
 
+process.on('uncaughtException', function (exception) {
+  console.log(exception); // to see your exception details in the console
+  // if you are on production, maybe you can send the exception details to your
+  // email as well ?
+});
+
+process.on('unhandledRejection', (reason, p) => { 
+    console.log("Unhandled Rejection at: Promise ", p, " reason: ", reason);
+    // application specific logging, throwing an error, or other logic here
+});
+
 export const app: Application = express();
 
 export const initializeExpressApp = () => {
